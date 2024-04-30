b881e32b619823e8de661c63d4d831a3a626edd3
José Emilio Munoz Dávila <jose-emilio.munoz-davila@campoint.net>
2024-04-30 11:16:34 +0200

    ampel logic

--- index.ino
+++ index.ino
@@ -1,44 +1,75 @@
 int switchState = 0;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(2, INPUT);
 
- // test
+const int greenLed  = 3;
+const int yellowLed = 4;
+const int redLed	= 5;
 
-}
+const int button = 2;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  switchState = digitalRead(2);
-  if (switchState == LOW) {
+void setup()
+{
+	pinMode(greenLed,  OUTPUT);
+	pinMode(yellowLed, OUTPUT);
+	pinMode(redLed,    OUTPUT);
+	pinMode(button,    INPUT);
 
-
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-  } else {
-
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-
-
-    delay(250);
-
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-
-
-    delay(250);
-  }
-
+    turnOnRedLed();
 }
 
 
-// Set-Location H:
-// cd '.\Eigene Dateien\LF7\Arduino\ampelOne\'
+void turnOffAllLeds()
+{
+    digitalWrite(greenLed,  LOW);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed,    LOW);
+}
 
+void turnOnRedLed()
+{
+    turnOffAllLeds();
+    digitalWrite(redLed, HIGH);
+}
 
+void turnOnYellowLed()
+{
+    turnOffAllLeds();
+    digitalWrite(yellowLed, HIGH);
+}
+
+void turnOnGreenLed()
+{
+    turnOffAllLeds();
+    digitalWrite(greenLed, HIGH);
+}
+
+void doTransitionRedToGreen()
+{
+    delay(1000);
+    turnOnYellowLed(),
+    delay(1000);
+    turnOnGreenLed();
+}
+
+void doTransitionGreenToRed()
+{
+    delay(1000);
+    turnOnYellowLed(),
+    delay(1000);
+    turnOnRedLed();
+}
+
+void loop()
+{  
+	switchState = digitalRead(button);
+
+	if(switchState == HIGH){ // button is pressed
+        if(digitalRead(redLed) == HIGH) {
+            doTransitionRedToGreen();
+            delay(5000);
+            doTransitionGreenToRed();
+        } else if(digitalRead(greenLed) == HIGH) {
+            delay(5000);
+            doTransitionGreenToRed();
+        }
+    }
+}	//go back to the beginning of the loop
